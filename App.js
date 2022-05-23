ac84172c731bde20167108171eea78e0d7fc6481
nablam <lamribenn@gmail.com>
2022-05-23 10:05:42 -0400

    makes a good json

---
+++ App.js
@@ -0,0 +1,42 @@
+import fetch from "node-fetch";
+//var data;
+//const fetch = require("node-fetch");
+
+//http://api.icndb.com/jokes/random
+//https://api.chucknorris.io/jokes/random
+
+const urlCategories = "https://api.chucknorris.io/jokes/categories";
+const chuckCategories = [
+  "animal",
+  "career",
+  "celebrity",
+  "dev",
+  "explicit",
+  "fashion",
+  "food",
+  "history",
+  "money",
+  "movie",
+  "music",
+  "political",
+  "religion",
+  "science",
+  "sport",
+  "travel",
+];
+
+const urlNorrisRand = "https://api.chucknorris.io/jokes/random/";
+const urlByCategory =
+  "https://api.chucknorris.io/jokes/random?category=" + chuckCategories[3];
+
+fetch(urlByCategory)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data.value.replace(/Chuck Norris/g, "Nabil"));
+    // console.log("secobd");
+  });
+
+// const numbs = [1, 2, 3, 4, 5, 7, 6, 8];
+// const sum = numbs.reduce((acc, curr) => acc + curr);
+
+// const over20 = numbs.filter((numbs) => numbs > 4);
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # TheJournal
+
+#
+
+# nice to have"
+
+Nabil norris api
+referring site will not work with iplogger since they will all show ip logger as ref site.
+#need to have
